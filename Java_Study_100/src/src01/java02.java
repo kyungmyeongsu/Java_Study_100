8b237762d16e3cbf3d7988e96499c94b0c085779
myeongsu <oplu92@gmail.com>
2022-03-15 00:26:56 +0900

    20220315

---
+++ Java_Study_100/src/src01/java02.java
@@ -0,0 +1,14 @@
+package src01;
+
+public class java02 { //java02는 클래스 명이다. java파일 명과 클래스 명은 동일해야 한다.
+
+	public static void main(String[] args) {
+		System.out.println("Hellow World~");
+	}
+}
+
+/*
+1. java는 대소문자의 구분이 중요하다.
+2. java는 ''말고 ""으로 사용해야한다.
+3. java는 ;으로 끝맺음을 해야한다.
+ */
