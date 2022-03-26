dadaedb2bc35d45057ca81e1d5cbe492d6088027
myeongsu <oplu92@gmail.com>
2022-03-27 00:34:46 +0900

    20220327 - 데이터 타입, 자료형, type

---
+++ Java_Study_100/src/src02/java01.java
@@ -0,0 +1,24 @@
+package src02;
+
+public class java01 {
+
+	public static void main(String[] args) {
+		System.out.println("데이터 타입");
+	}
+
+}
+
+
+/*
+[1] : 크게 봤을 때 --> 기본형 타입, 참조형 타입
+
+[2] : 기본형 타입(Primtive Data Type) --> 8개
+정수형 --> byte(1byte), short(2), int(4), long(8)
+실수형 --> float(4), double(8)
+문자형 --> char(2byte) --> 문자 한개 --> 참고로, 문자열을 다루는 타입은 없다.
+부울형(논리형:참 or 거짓) --> boolean(1byte) --> true, false
+
+[3] : 참조형 타입(Reference Data Type) --> 위 기본형에 속하지 않는 데이터 형들.
+대표적인 것들 --> 클래스(class), 배열(array), 인터페이스(interface), 문자열(String/immutable)
+참조형 변수의 특징 --> 데이터가 저장된 메모리의 주소 값을 저장하는 변수이다.
+*/
