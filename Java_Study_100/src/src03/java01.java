43a04c40dc617b1cfddf1a5ed0ccbe4dfc68a19b
kyungmyeongsu <oplu92@gmail.com>
2022-10-27 18:16:12 +0900

    20221027 if문

---
+++ Java_Study_100/src/src03/java01.java
@@ -0,0 +1,17 @@
+package src03;
+
+public class java01 {
+
+	public static void main(String[] args) {
+		// [1] : 변수 선언
+		int kor=80, eng=90, math=100;
+		
+		// [2] : if 조건문만 사용하기
+		if ( kor >= 80 )
+			System.out.println( "당신의 국어 점수는 B 학점입니다." );
+		
+		System.out.println( "학점이 안나왔다면 80점이 안되서입니다." );
+		
+	}
+
+}
