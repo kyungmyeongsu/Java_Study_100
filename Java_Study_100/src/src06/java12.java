d023c63107d15b4fc11f52d0a5bd6dda7c59214c
myeongsu <oplu92@gmail.com>
2022-12-19 23:39:09 +0900

    20221219 getter,setter,클래스,상속

--- Java_Study_100/src/src06/java12.java
+++ Java_Study_100/src/src06/java12.java
@@ -12,6 +12,13 @@ class Person3 {
 	private int weight;
 	
 	// Constructor
+	Person3() {}
+	Person3( String name, int age, int height, int weight ) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
 	
 	// Method
 	public String getName() { return name; }
@@ -25,9 +32,70 @@ class Person3 {
 	
 	public int getWeight() { return weight; }
 	public void setWeight( int weight ) { this.weight = weight; }
+	
+	public void move() {
+		System.out.println("이동중..");
+	}
 }
 
-class Villain1 extends Person3 {}
+class Villain1 extends Person3 {
+	// Field
+	private String unique_key;
+	private int weapon;			// 1~9 숫자로 무기 분류 --> 1: 창, 2: 방패, 3: 총..
+	private double power;
+	
+	// Constructor
+	Villain1() {}
+	Villain1( String name, int age, int height, int weight, String unique_key, int weapon, double power ) {
+		super( name, age, height, weight );
+		this.unique_key = unique_key;
+		this.weapon = weapon;
+		this.power = power;
+	}
+	
+	// Method
+	public String getUnique_key() { return unique_key; }
+	public void setUnique_key( String unique_key ) { this.unique_key = unique_key; }
+	
+	public int getWeapon() { return weapon; }
+	public void setWeapon( int weapon ) { this.weapon = weapon; }
+	
+	public double getPower() { return power; }
+	public void setPower( double power ) { this.power = power; }
+	
+	// printPerson()
+	public void printPerson() {
+		System.out.println( "---------------------------------" );
+		System.out.println( "악당 이름 : " + getName() );
+		System.out.println( "악당 나이 : " + getAge() );
+		System.out.println( "악당의 키 : " + getHeight() );
+		System.out.println( "악당 체중 : " + getWeight() );
+		System.out.println( "악당 넘버 : " + getUnique_key() );
+		System.out.println( "악당 무기 : " + getWeaponName(getWeapon()) );  // 숫자(1~9) --> 1: 창, 2: 방패, 3: 총..
+		System.out.println( "악당 파워 : " + getPower() );
+		System.out.println( "---------------------------------" );
+	}
+	
+	// getWeaponName()
+	public String getWeaponName( int a ) {
+		String weapon;
+		switch( a ) {
+			case 1: 
+				weapon = "창";
+				break;
+			case 2:
+				weapon = "방패";
+				break;
+			case 3:
+				weapon = "총";
+				break;
+			default:
+				weapon = "---";
+				break;
+		}
+		return weapon;
+	}
+}
 
 class Hero1 extends Person3 {}
 
@@ -37,7 +105,26 @@ public class java12 {
 		// [1] : 객체 생성
 		Person3 p1 = new Person3();
 		p1.setName( "홍길동" );
+		p1.move();
 		System.out.println( p1.getName() );
+		
+		// [1] : 객체 생성
+		Villain1 v1 = new Villain1( "좀비", 20, 180, 80, "15001231", 1, 99.5 );
+		v1.printPerson();
+		System.out.print( v1.getName() + " " );
+		v1.move();  // 부모 클래스의 move() 메서드를 호출.
+		
+		// [2] : 객체 생성
+		Villain1 v2 = new Villain1( "도깨비", 30, 175, 70, "11001121", 1, 77.5 );
+		v2.printPerson();
+		System.out.print( v2.getName() + " " );
+		v2.move();
+
+		// [3] : 객체 생성
+		Villain1 v3 = new Villain1( "몽달귀신", 40, 150, 40, "10001010", 3, 27.5 );
+		v3.printPerson();
+		System.out.print( v3.getName() + " " );
+		v3.move();
 		
 	}
 
