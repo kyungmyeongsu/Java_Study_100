8e2ae3af445acaa05a05cfefd2e9977eee6de87a
myeongsu <oplu92@gmail.com>
2022-12-25 23:05:53 +0900

    20221225 다형성,클래스,호출

--- Java_Study_100/src/src07/java07.java
+++ Java_Study_100/src/src07/java07.java
@@ -13,6 +13,10 @@ class Student2 extends Person2 {
 	String str2 = "난 자식 클래스";
 	void method1() { System.out.println( "오버라이딩 - AAA" ); }
 	void sss() { System.out.println( "sss" ); }
+	void x() {
+		method1();
+		super.method1();
+	}
 }
 
 public class java07 {
@@ -50,6 +54,21 @@ public class java07 {
 		
 		// [4] : 객체 생성
 		// Student bbb = new Person();	// Err
+		
+		
+		// [1] : 객체 생성 --> 부모 + 자식 클래스의 모든 자원을 다 쓸 수 있다.
+		// [ ! ] : 그런데 말이죠.. --> 자식 클래스에서 오버라이딩된 부모 클래스의 원본 메서드를 호출하고 싶다면? --> super 사용.
+		Student2 s3 = new Student2();
+		System.out.println( "------------------------[super 사용]" );
+		s3.x();
+		System.out.println( "------------------------[super 사용]" );
+
+		// [2] : 객체 생성 --> 범위는 부모의 자원만을 쓸 수 있다.(?)
+		// [ ! ] : 그런데 말이죠.. --> 자식의 메서드를 바로 호출하고 싶다면? --> 캐스트 필요.
+		Person2 s4 = new Student2();
+		System.out.println( "------------------------[캐스트 사용]" );
+		((Student2) s4).sss();
+		System.out.println( "------------------------[캐스트 사용]" );
 	}
 
 }
